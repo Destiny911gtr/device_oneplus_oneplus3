f493e812d74c3282665ab77cf3a49dff0606af13
ZeNiXxX <vitjahermann@gmail.com>
2018-01-31 19:08:12 +0100

    op3: add facelock properties
    Change-Id: Ic9df82d841c052780408ffa4457177535a3b69ec

--- device.mk
+++ device.mk
@@ -496,3 +496,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     AdAway \
     PixelLauncher
+
+# facelock properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.facelock.black_timeout=700 \
+    ro.facelock.det_timeout=2500 \
+    ro.facelock.rec_timeout=3500 \
+    ro.facelock.est_max_time=600
